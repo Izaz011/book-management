23d44ca167e93620a0e75cb2015e81c27c364108
bbiplab165 <bbiplab165@gmail.com>
2022-11-28 15:03:08 +0530

    Models added

---
+++ src/models/bookModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const bookschema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    excerpt: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: ObjectId,
+        required: true,
+        refs: 'User'
+    },
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: String,
+        required: true
+    },
+    reviews: {
+        type: Number,
+        default: 0
+    },
+    deletedAt: {
+        type: Date//, when the document is deleted 
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+        required: true
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model("Book", bookschema)
